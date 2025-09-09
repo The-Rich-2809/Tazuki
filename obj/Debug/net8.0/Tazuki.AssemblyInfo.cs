6b7b356de948a87d2975f4be3436281a1f1b3a47
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2025-09-09 02:11:39 -0600

    4to
    Cambio a modo claro

--- obj/Debug/net8.0/Tazuki.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tazuki.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tazuki")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc360c400d8a60d1f4522f522922ed3b8ba9ba65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3243bb906aa75bbd242f6016b122bfeb86b27930")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tazuki")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tazuki")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
