1c85a3054725be6eb2a5c5130962925c350ac426
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2025-10-14 15:47:34 -0600

    35

--- obj/Debug/net8.0/Tazuki.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tazuki.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tazuki")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe25a0da55fef20781afcfe1e183386df5bc77d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4968839a2219432f0ca735f870cdcf1329a4e6cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tazuki")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tazuki")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
